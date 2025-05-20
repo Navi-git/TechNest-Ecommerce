cccf00ae008ae6c19f94b6ab748796f4ab875faa
Naveen <crownnaveens@gmail.com>
2025-05-20 13:31:15 +0530

    Secret key secured in env.

--- TechNest/settings.py
+++ TechNest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3o2c4_=ra$@z5mg(04v)dhl(mx-_2(bv@z)a-@r(23vbc)5zk1'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
